2b8175d151461974ecc7dd14be17b4fd1da5c31b
DannJohnrem <jonrhem10@gmail.com>
2022-08-29 01:23:51 +0800

    Update DashboardController.php

--- app/Http/Controllers/Admin/Dashboard/DashboardController.php
+++ app/Http/Controllers/Admin/Dashboard/DashboardController.php
@@ -7,6 +7,19 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    /**
+     * The `__construct()` function is a special function that is called when a class is instantiated.
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * It returns the view `admin.dashboard.index`
+     *
+     * @return The view file located at resources/views/admin/dashboard/index.blade.php
+     */
     public function index()
     {
         return \view('admin.dashboard.index');
